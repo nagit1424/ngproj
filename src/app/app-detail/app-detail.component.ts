6a97d539fe4dbac886c0093f044798810b40d1b8
Kovuri <nkovuri@deloitte.com>
2019-02-14 15:52:13 +0530

    push code

---
+++ src/app/app-detail/app-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-detail',
+  templateUrl: './app-detail.component.html',
+  styleUrls: ['./app-detail.component.scss']
+})
+export class AppDetailComponent implements OnInit {
+  name="detail page";  
+  constructor() { 
+
+    
+  }
+
+  ngOnInit() {
+
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppDetailComponent } from './app-detail/app-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppDetailComponent
   ],
   imports: [
     BrowserModule,
